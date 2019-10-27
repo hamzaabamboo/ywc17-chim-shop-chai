0ac4b13bca53ce4bc60fc2db81c1738fa82cf0f5
Tanyawat Vittayapalotai <hamzaabamboo@gmail.com>
2019-10-27 17:43:54 +0700

    Updates

---
+++ precache-manifest.ab2286db6226b575c990e68ca194cffa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5499b247dc2c349c8c15d2a3818d3a6b",
+    "url": "/ywc17-chim-shop-chai/index.html"
+  },
+  {
+    "revision": "e66d596703f397d3aa4e",
+    "url": "/ywc17-chim-shop-chai/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "36742dbf1566b17c9c56",
+    "url": "/ywc17-chim-shop-chai/static/css/main.599f463a.chunk.css"
+  },
+  {
+    "revision": "e66d596703f397d3aa4e",
+    "url": "/ywc17-chim-shop-chai/static/js/2.97dfec03.chunk.js"
+  },
+  {
+    "revision": "36742dbf1566b17c9c56",
+    "url": "/ywc17-chim-shop-chai/static/js/main.8559f135.chunk.js"
+  },
+  {
+    "revision": "dff40fe7063d076d627c",
+    "url": "/ywc17-chim-shop-chai/static/js/runtime-main.029d9861.js"
+  },
+  {
+    "revision": "45415b3d8382c9a11e16af5f88efab0e",
+    "url": "/ywc17-chim-shop-chai/static/media/TATSanaChon-Bold.45415b3d.woff2"
+  },
+  {
+    "revision": "29c3fec8f8a81165e4a924a4f50df8d9",
+    "url": "/ywc17-chim-shop-chai/static/media/TATSanaChon-Regular.29c3fec8.woff2"
+  },
+  {
+    "revision": "f51f1b81ee331401e0118c8e97c18083",
+    "url": "/ywc17-chim-shop-chai/static/media/TATSanaSuksa-Regular.f51f1b81.woff2"
+  }
+]);
